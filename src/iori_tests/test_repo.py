a67b2b89f960922ec0d1b72243e8d7f37d371397
Kouhei Maeda <mkouhei@palmtb.net>
2014-04-09 12:40:49 +0900

    Update pre-commit script.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- src/iori_tests/test_repo.py
+++ src/iori_tests/test_repo.py
@@ -20,32 +20,3 @@ class RepoTests(unittest.TestCase):
         self.assertEquals('/tmp/iori_test/', self.r.dirpath)
         self.assertIsNone(self.r.git_repo)
         self.assertFalse(self.r.contname)
-
-    '''
-    def test_check_dirrepo(self):
-        unittest.TextTestRunner().run(self.r.check_dirrepo())
-        '''
-
-    def test_write_file(self):
-        True
-
-    def test_git_init(self):
-        True
-
-    def test_git_add_commit(self):
-        True
-
-    def test_make_branch(self):
-        True
-
-    def test_checkout_branch(self):
-        True
-
-    def test_branches(self):
-        True
-
-    def test_branch_files(self):
-        True
-
-    def git_rm_commit(self):
-        True
